2fc1d5b78f6fea146662101df82d4ddf7443762d
moin2672 <moin2672@gmail.com>
2020-12-07 11:29:46 +0530

    fourth commit-image upload

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto'); //to generate the file names
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 
-const app=express();
+const app = express();
 
+//MiddleWare
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 //Setting View Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/',(req, res)=>{
+//Mongo URI
+const MongoURI = "mongodb+srv://meanapp:AEKzArBnCBP1ehTH@cluster0.fgaaw.mongodb.net/mongouploads";
+
+//Create Mongo connection
+const conn = mongoose.createConnection(MongoURI);
+
+//Initialize gfs - ref: https://github.com/aheckmann/gridfs-stream
+let gfs;
+conn.once('open', () => {
+    //Init Stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+    // all set!
+})
+
+//Create Storage Engine - ref: https://github.com/devconcept/multer-gridfs-storage
+const storage = new GridFsStorage({
+    url: MongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+// @route GET /
+// @desc Loads form
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-const port=5000;
+// @route POST /upload
+// @desc Uploads file to DB - upload.single('file') -> middleware with field name as file
+app.post('/upload',upload.single('file'), (req, res)=>{
+    res.json({file:req.file});
+})
 
-app.listen(port,()=>console.log(`Server started on port ${port}`));
+const port = 3500;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
