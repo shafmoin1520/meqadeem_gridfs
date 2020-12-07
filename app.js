67101b478caf24e3fcba1a1cfa2183009e4d923a
moin2672 <moin2672@gmail.com>
2020-12-07 15:07:21 +0530

    eighth commit-DISPLAY IMAGE - GET - All file

--- app.js
+++ app.js
@@ -55,7 +55,24 @@ const upload = multer({ storage });
 // @route GET /
 // @desc Loads form
 app.get('/', (req, res) => {
-    res.render('index');
+    // res.render('index');
+    gfs.files.find().toArray((err, files)=>{
+        //Check if files
+        if(!files || files.length === 0){
+            // return res.status(404).json({err:'No files exists'});
+            res.render('index',{files:false});
+        }else{
+            files.map(file=>{
+                if(file.contentType==='image/jpeg'|| file.contentType==='image/png'){
+                    file.isImage=true;
+                }else{
+                    file.isImage=false;
+                }
+            });
+            res.render('index',{files:files});
+        }
+
+    });
 });
 
 // @route POST /upload
