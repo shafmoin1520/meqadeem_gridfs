ccc90d12a5ae1974ebc0a24b2e000d9768ecdcf5
moin2672 <moin2672@gmail.com>
2020-12-07 13:08:45 +0530

    sixth commit-JSON - GET - single files

--- app.js
+++ app.js
@@ -79,6 +79,20 @@ app.get('/files', (req, res)=>{
     });
 });
 
+// @route GET /files/:filename
+// @desc Display single file in JSON
+app.get('/files/:filename', (req, res)=>{
+    gfs.files.findOne({filename:req.params.filename},(err,file)=>{
+        //Check if file
+        if(!file || file.length === 0){
+            return res.status(404).json({err:'No file exists'});
+        }
+
+        //File exists
+        return res.json({file});
+    });
+});
+
 const port = 3500;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
